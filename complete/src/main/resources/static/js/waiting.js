cb9ed70cff1dbbbf524eba220026fcd9af8f0b68
Sean Shmulevich <seanshmulevich@seans-mbp-3.lan>
2022-11-23 05:57:12 -0500

    this one

--- complete/src/main/resources/static/js/waiting.js
+++ complete/src/main/resources/static/js/waiting.js
@@ -1,17 +1,19 @@
         //when you page and come back this code will be executed and it allows for the question to be correct even if the user
         //goes to like check instagram or something after the question has already been posted
         document.onvisibilitychange = async function() {
+            let q = await (await fetch("/getProjectNames")).json();
+            let currentTime = q["questionNumber"];
+            localStorage.setItem('currentTime', q["questionNumber"]);
 
             if (document.visibilityState === 'visible') {
                 //reload when the user leaves and come back again.
                 window.location.reload();
             }
-            else{
+            else if(document.visibilityState === 'hidden'){
                             //i dont know or think i need this re-load.
             //there is a fetch every time the user leaves the page and comes back.
             let q = await (await fetch("/getProjectNames")).json();
-            let currentTime = q["questionNumber"];
-            localStorage.setItem('currentTime', q["questionNumber"]);
+            let currentTime = localStorage.getItem('currentTime');
             //only do anything really if the question is currently not been answered and the time is still not -1
             if(currentTime !== -1 && localStorage.getItem(`user-voted-${q["content"]}`) !== "true"){
                 localStorage.setItem('currentQuestion', q["content"]);
