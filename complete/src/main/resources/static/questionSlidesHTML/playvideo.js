2df179f628d3a8c7e43b394bdc29592ff9a6713e
Sean Shmulevich <seanshmulevich@seans-mbp-3.lan>
2022-11-23 09:17:42 -0500

    adding youtube

--- complete/src/main/resources/static/questionSlidesHTML/playvideo.js
+++ complete/src/main/resources/static/questionSlidesHTML/playvideo.js
@@ -3,10 +3,10 @@ let times = 0;
 window.addEventListener("load", () => {
     let path = window.location.pathname;
     let page = path.split("/").pop();
-    //gets the number from the name of the page. 
-    // let pageNumber = parseInt(page);
-    // if (times == 0) {
-    //     playY = Player.src += "?autoplay=1";
-    //     times = 1;
-    // }
+    // gets the number from the name of the page. 
+    let pageNumber = parseInt(page);
+    if (times == 0) {
+        playY = Player.src += "?autoplay=1";
+        times = 1;
+    }
 });
